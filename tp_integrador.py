09e67627be6bbd3e722c6f105a44540e94f540f7
CamiTorresCT <camitorresvm2@gmail.com>
2025-07-28 09:59:25 -0300

    Add files via upload

---
+++ tp_integrador.py
@@ -0,0 +1,41 @@
+def registrar_visitas():
+    visitas = []
+
+    for i in range(3):
+        print(f"\nRegistro #{i+1}")
+        nombre = input("Nombre del estudiante: ")
+        curso = input("Curso: ")
+        motivo = input("Motivo por el que entra a la MakerSpace: ")
+
+        visita = {
+            "nombre": nombre,
+            "curso": curso,
+            "motivo": motivo
+        }
+        visitas.append(visita)
+
+    return visitas
+
+def mostrar_datos(visitas):
+    print("\n--- Lista de estudiantes que ingresaron a la MakerSpace ---")
+    for visita in visitas:
+        print(f"{visita['nombre']} - {visita['curso']} - Motivo: {visita['motivo']}")
+
+    cursos = {}
+    for visita in visitas:
+        curso = visita["curso"]
+        if curso in cursos:
+            cursos[curso] += 1
+        else:
+            cursos[curso] = 1
+
+    print("\n--- Cantidad de visitas por curso ---")
+    for curso, cantidad in cursos.items():
+        print(f"{curso}: {cantidad} estudiante(s) ingresaron")
+
+    print("\nPrimer estudiante que ingresó a la MakerSpace:")
+    print(visitas[0]["nombre"])
+
+def main():
+    visitas = registrar_visitas()
+    mostrar_datos(visitas)
